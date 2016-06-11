2125faf02429f34bc41b5ef76424ff596663ff6f
mymy91 <tmdinh91@gmail.com>
2016-06-11 22:22:32 +0700

    Python Projects for Kids
    first book

---
+++ 110616.py
@@ -0,0 +1,47 @@
+# This is my first function called name. It will ask the name and
+# print a message.
+
+def SeperateLession():
+	print('--END--')
+	print('')
+	
+
+#Lession 1
+print('--Lession 1--')
+def name():
+	firstName = input('What is your name?')
+	print('So nice to meet you, ' + firstName)
+	
+name()
+SeperateLession()
+
+#Lession 2
+print('--Lession 2--')
+def addition():
+	firstNumber = 30
+	secondNumber = 60
+	print(firstNumber + secondNumber)
+	
+addition()
+SeperateLession()
+
+#Lession 3
+print('--Lession 3--')
+def addition_Integer():
+	firstNumber = int(input('Input the firstNumber:'))
+	secondNumber = int(input('Input the secondNumber:'))
+	print(firstNumber + secondNumber)
+	
+addition_Integer()
+SeperateLession()
+
+#Lession 4
+print('--Lession 4--')
+def addition_Float():
+	firstNumber = float(input('Input the firstNumber:'))
+	secondNumber = float(input('Input the secondNumber:'))
+	print(firstNumber + secondNumber)
+
+addition_Float()
+SeperateLession()	
+	
